ef2ab9a9b708475d77dff761234485390285e87f
deepak25888 <deepak25888@gmail.com>
2020-08-09 08:21:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World",8+2,"total 10 log system");
+alert("Hii");
+document.write("<b>My Name is Deepak nair</b>");
+console.warn("This is a warning");
+console.error("This is an error");
+console.assert(5==5);
+
+var a;
+console.log(a);
+
